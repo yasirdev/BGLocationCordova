af02fb4c81aba43b12cb1dbfbd1043395840de02
Yasir Rajpoot <YasirRajpoot@Yasir.local>
2016-11-04 20:17:40 +0500

    adding all files in the first commit

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-mauron85-background-geolocation.backgroundGeolocation",
+        "file": "plugins/cordova-plugin-mauron85-background-geolocation/www/backgroundGeolocation.js",
+        "pluginId": "cordova-plugin-mauron85-background-geolocation",
+        "clobbers": [
+            "backgroundGeolocation"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-plugin-mauron85-background-geolocation": "2.2.4"
+};
+// BOTTOM OF METADATA
+});
